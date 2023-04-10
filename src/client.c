240f0cc5d526cb56be2ec0504ba37cd83c91c4b6
Zoh-j02r <saulobruno@usp.br>
2023-04-10 20:00:40 -0300

    feat: added timer counter

--- src/client.c
+++ src/client.c
@@ -4,8 +4,13 @@
 char input_buffer[256] = {0};
 char client[MAX_CLIENT_NAME_SIZE];
 int sock;
+int locked = 0;
+int row, col;
 WINDOW *input_win; 
+WINDOW *wait_win; 
 WINDOW *chat_win;
+
+int counter = 0;
 	//TODO:
 	// -- add function for checking user parameters
 	// -- add support for special characters
@@ -14,55 +19,95 @@ WINDOW *chat_win;
 	// the availave space
 	// -- add btop like windows
 
-void handle_new_message(int sock) {
+void handle_new_message(int sock)
+{
 	char buffer[256] = {0};
 	int valread = read(sock, buffer, 256);
-	
 	if (valread > 0) 
 	{
-	    	wprintw(chat_win,"%s\n",buffer);
+		if (counter > row - 2 ){
+    		wclear(wait_win);
+	   		wrefresh(chat_win); 
+			counter = 0;
+		}
+		counter++;
+	    wprintw(chat_win,"%s\n",buffer);
 	   	wrefresh(chat_win); 
 	}
 }
 
+void handle_wait_window(int time_delay)
+{
+	wait_win = newwin(1, col, row-1, 0);
+	int block = (col - 19)/time_delay;
+	wprintw(wait_win,"Enviando mensagem [");
+	for (int i = 0; i < block*time_delay ; i++)
+	{
+    	wmove(wait_win, 0, 19+i);
+ 		wprintw(wait_win," ");	
+ 	}
+ 	wprintw(wait_win," ]");	
+	wrefresh(wait_win); 
+	int a = 0;
+	while(time_delay > 0)
+	{
+       	wmove(wait_win, 0, 19 + a);
+		for (int i = 0; i < block; i++)
+		{
+       		wmove(wait_win, 0, 19 + a + i);
+			waddch(wait_win, ACS_BLOCK);
+		}
+		a = a + block;
+		waddch(wait_win, ACS_BLOCK);
+		wrefresh(wait_win);
+        sleep(1);
+        time_delay--;
+	}
+	locked = 0;
+    wclear(wait_win);
+    werase(wait_win);
+    wrefresh(input_win);
+}
+
 void *input_thread(void *arg) {
 	int index = 0; 
-    	char buffer[256];
+    char buffer[256];
 	while(1)
 	{
  		int ch = wgetch(input_win); 
-        	if (ch == '\n') 
-        	{
-        	    	wprintw(chat_win,"%s %s\n",client,buffer);
-        	    	wrefresh(chat_win);
-		    	strncpy(input_buffer, buffer, sizeof(input_buffer) - 1);
-		    	input_buffer[sizeof(input_buffer) - 1] = '\0'; 
-		    	memset(buffer, 0, sizeof(buffer)); 
-        	    	index = 0;
-        	    	wclear(input_win); 
-        	    	wprintw(input_win, "%s",client); 
-        	    	wrefresh(input_win); 
-        	}
-        	else if (ch == KEY_BACKSPACE || ch == 127) 
-        	{
-        		if (index > 0) 
-        		{
-        			index--; 
-        			wmove(input_win, 0, strlen(client)+index); 
-        			wprintw(input_win, " "); 
-        			wmove(input_win, 0, strlen(client)+index); 
-        			wrefresh(input_win); 
-        		}
-        	}
-        	else if (ch >= 32 && ch <= 126) 
-        	{
-        		if (index < (int)sizeof(buffer)-1) 
-        		{
-        			buffer[index++] = ch; 
-        			wprintw(input_win, "%c", ch); 
-        			wrefresh(input_win); 
-        		}
-        	}
+		if (ch == '\n') 
+    	{
+    		wprintw(chat_win,"%s %s\n",client,buffer);
+    		wrefresh(chat_win);
+	    	strncpy(input_buffer, buffer, sizeof(input_buffer) - 1);
+	    	input_buffer[sizeof(input_buffer) - 1] = '\0'; 
+	    	memset(buffer, 0, sizeof(buffer)); 
+    		index = 0;
+    		wclear(input_win);
+			handle_wait_window(10);
+    		wprintw(input_win, "%s",client); 
+    		wrefresh(input_win);
+    	}
+    	else if (ch == KEY_BACKSPACE || ch == 127) 
+    	{
+    		if (index > 0) 
+    		{
+    			index--; 
+    			wmove(input_win, 0, strlen(client)+index); 
+    			wprintw(input_win, " "); 
+    			wmove(input_win, 0, strlen(client)+index); 
+    			wrefresh(input_win); 
+    		}
+    	}
+    	else if (ch >= 32 && ch <= 126) 
+    	{
+    		if (index < (int)sizeof(buffer)-1) 
+    		{
+    			buffer[index++] = ch; 
+    			wprintw(input_win, "%c", ch); 
+    			wrefresh(input_win); 
+    		}
+    	}
 	}
     return NULL;
 }
@@ -70,20 +115,23 @@ void *input_thread(void *arg) {
 void *send_thread(void *arg) {
 	char buffer[256] = {0};
 	while (1) {
-		while (strlen(input_buffer) == 0) 
-		{
-			usleep(1000); 
-		}
-		// Copy input buffer to send buffer
-		strncpy(buffer, input_buffer, sizeof(buffer) - 1);
-		buffer[sizeof(buffer) - 1] = '\0'; // Ensure null termination
-		memset(input_buffer, 0, sizeof(input_buffer)); 
-		// Send data to server
-		if (send(sock, buffer, strlen(buffer), 0) == -1) {
-		    printf("\nSend failed \n");
-		    break;
-		}
-		memset(buffer, 0, sizeof(buffer));
+			//if (locked)
+			//{
+			//	handle_wait_window(10);
+			//	usleep(100000);
+			//}
+			while (strlen(input_buffer) == 0) 
+			{
+				usleep(1000); 
+			}
+			strncpy(buffer, input_buffer, sizeof(buffer) - 1);
+			buffer[sizeof(buffer) - 1] = '\0';
+			memset(input_buffer, 0, sizeof(input_buffer)); 
+			if (send(sock, buffer, strlen(buffer), 0) == -1) {
+			    printf("\nSend failed \n");
+			    break;
+			}
+			memset(buffer, 0, sizeof(buffer));
 	}
 	return NULL;
 }
@@ -97,15 +145,6 @@ int main(int argc, const char* argv[])
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE); 
-	
-	start_color(); // enable color support
-	init_pair(1, COLOR_RED, COLOR_BLACK); 
-	init_pair(2, COLOR_GREEN, COLOR_BLACK); 
-	init_pair(3, COLOR_BLUE, COLOR_BLACK); 
-	init_pair(4, COLOR_MAGENTA, COLOR_BLACK); 
-	init_pair(5, COLOR_YELLOW, COLOR_BLACK); 
-	
-	int row, col;
 	getmaxyx(stdscr, row, col); 
 	input_win = newwin(1, col, row-1, 0);
 	chat_win = newwin(row-1, col, 0, 0);
@@ -158,20 +197,19 @@ int main(int argc, const char* argv[])
 	// Send messages to server
 	while (1) 
 	{
-	    	fd_set set;
-	    	FD_ZERO(&set);
-	    	FD_SET(sock, &set);
-	    	struct timeval timeout = {0, 0};
-	    	// Wait for new message from server
-	    	int ret = select(sock + 1, &set, NULL, NULL, &timeout);
-	    	if (ret > 0) {
-	        	handle_new_message(sock);
-	    	}
+		fd_set set;
+		FD_ZERO(&set);
+		FD_SET(sock, &set);
+		struct timeval timeout = {0, 0};
+		// Wait for new message from server
+		int ret = select(sock + 1, &set, NULL, NULL, &timeout);
+		if (ret > 0) {
+	    	handle_new_message(sock);
+		}
 	}
 	// Close socket
 	close(sock);
 	pthread_join(input_tid, NULL);
 	pthread_join(send_tid, NULL);
 	return 0;
-	
 }
